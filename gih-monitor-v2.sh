3007b00a11e8a6f96af3d97085188e856a1a4566
alper.bassarac <alper.bassarac@turknet.net.tr>
2016-08-08 15:27:47 +0300

    update DNS addresses

--- gih-monitor-v2.sh
+++ gih-monitor-v2.sh
@@ -7,35 +7,35 @@ function check_domain() {
     # $2 = domain
     case "$1" in
         aile)
-            dns="193.192.98.20"
+            dns="193.192.98.59"
             yip="193.192.98.49"
         ;;
         aile-oyun)
-            dns="193.192.98.55"
+            dns="193.192.98.51"
             yip="193.192.98.46"
         ;;
         aile-sohbet)
-            dns="193.192.98.57"
+            dns="193.192.98.54"
             yip="193.192.98.47"
         ;;
         aile-sosyal)
-            dns="193.192.98.52"
+            dns="193.192.98.56"
             yip="193.192.98.48"
         ;;
         aile+oyun+sohbet)
-            dns="193.192.98.56"
+            dns="193.192.98.52"
             yip="193.192.98.42"
         ;;
         aile+sosyal+oyun)
-            dns="193.192.98.54"
+            dns="193.192.98.57"
             yip="193.192.98.43"
         ;;
         aile+sohbet+sosyal)
-            dns="193.192.98.51"
+            dns="193.192.98.55"
             yip="193.192.98.45"
         ;;
         aile+oyun+sohbet+sosyal)
-            dns="193.192.98.59"
+            dns="193.192.98.20"
             yip="193.192.98.41"
         ;;
         cocuk)
