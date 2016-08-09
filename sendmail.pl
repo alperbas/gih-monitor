3b0da49abbbbd8b82604cd0d0a35daf7738e4690
alper.bassarac <alper.bassarac@turknet.net.tr>
2016-08-09 10:45:42 +0300

    update address

--- sendmail.pl
+++ sendmail.pl
@@ -10,9 +10,7 @@ my $data = $body;
 
 my $msg = MIME::Lite->new(
     From    => 'gih-monitor@turk.net',
-#    To      => 'mdy@turknet.net.tr, networkoperasyon@turknet.net.tr, noc@turknet.net.tr',
-    To      => 'ersin.ozdemir@turknet.net.tr, ahmet.coban@turknet.net.tr, murat.oguz@turknet.net.tr, busra.keles@turknet.net.tr',
-    Cc      => 'vahit.tabak@turknet.net.tr, alper.bassarac@turknet.net.tr',
+    To      => 'mdy@turknet.net.tr, networkoperasyon@turknet.net.tr, noc@turknet.net.tr',
     Subject => 'Guvenli Internet Hizmeti Gunluk Kontrol Raporu',
     Type    => 'text/html',
     Data    => $data
